d7a7978da256613bb10a532ea80c8e34ede68a23
Mark Verbeek <markverbeek@mac.com>
2020-07-31 12:25:20 +0200

    show screams

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import Grid from '@material-ui/core/Grid'
 
 export class home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>homepage</h1>
-            </div>
-        )
+    state = {
+        screams: null
     }
+    componentDidMount(){
+        axios
+        .get('https://europe-west1-socialape-d3a4c.cloudfunctions.net/api/screams')
+        .then(res => {
+            console.log(res.data)
+            this.setState({
+                screams: res.data
+            })
+        })
+        .catch(err => console.log(err));
+    }
+
+    render() {
+        let recentScreamsMarkup = this.state.screams ? (
+            this.state.screams.map(scream => <p>{scream.body}</p>)
+        ) : <p>loading...</p>
+        return (
+           <Grid container spacing={16}>
+               <Grid item sm={8} xs={12}>
+                   {recentScreamsMarkup}
+               </Grid>
+               <Grid item sm={4} xs={12}>
+                   <p>Profile...</p>
+               </Grid>
+           </Grid>
+        )
+    } 
 }
 
 export default home
