2d05f173e36d74c71811c226a3f3097a7101c379
Mark Verbeek <markverbeek@mac.com>
2020-08-07 13:04:29 +0200

    login button spinner and general error message

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,126 +1,153 @@
-import React, { Component } from 'react'
-import withStyles from '@material-ui/core/styles/withStyles';
-import PropTypes from 'prop-types'
-import AppIcon from '../images/apeicon.png'
-import axios from 'axios'
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from "prop-types";
+import AppIcon from "../images/apeicon.png";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 //MUI Stuff
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography'
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-const styles={ 
-    form: {
-        textAlign: 'center'
-    },
-    image: {
-        margin: '20px auto 20px auto'
-    },
-    pageTitle:{
-        margin: '10px auto 10px auto'
-    },
-    textField: {
-        margin: '10px auto 10px auto'
-    },
-    button: {
-        marginTop: 20
-    }
+const styles = {
+  form: {
+    textAlign: "center",
+  },
+  image: {
+    margin: "20px auto 20px auto",
+  },
+  pageTitle: {
+    margin: "10px auto 10px auto",
+  },
+  textField: {
+    margin: "10px auto 10px auto",
+  },
+  button: {
+    marginTop: 20,
+    position: "relative",
+  },
+  customError: {
+    color: "red",
+    fontSize: "0.8rem",
+    marginTop: 10,
+  },
+  progress: {
+    position: "absolute",
+  },
+};
 
-
-}
-
-
-
-export class login  extends Component {
-    constructor(){
-        super();
-        this.state = {
-            email: '',
-            password: '',
-            loading: false,
-            errors: {}
-        }
-    }
-    handleSubmit = (event) => {
-        event.preventDefault();
-        this.setState({
-            loading: true
-        });
-        const userData= {
-            email: this.state.email,
-            password: this.state.password
-        }
-        axios.post('https://europe-west1-socialape-d3a4c.cloudfunctions.net/api/login', userData)
-        .then(res => {
-            console.log(res.data);
-            this.setState({
-                loading: false
-            });
-            this.props.history.push('/');
-            })
-            .catch(err => {
-                console.log(err)
-                this.setState({
-                    errors: err.response.data,
-                    loading: false     
-                })
-            });
-      
-
-
-
+export class login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      errors: {},
     };
-    handleChange = (event)=> {
-        this.setState({[event.target.name]: event.target.value})
-    }
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+    });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    axios
+      .post(
+        "https://europe-west1-socialape-d3a4c.cloudfunctions.net/api/login",
+        userData
+      )
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          loading: false,
+        });
+        this.props.history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({
+          errors: err.response.data,
+          loading: false,
+        });
+      });
+  };
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-    render() {
-        const{ classes } = this.props;
-        const { errors, loading } = this.state;
-        return (
-            <Grid container className={classes.form}>
-                <Grid item sm/>
-                <Grid item sm> 
-                    <img src={AppIcon} alt="monkey" className={classes.image}/>
-                    <Typography variant="h2" className={classes.pageTitle}>
-                        Login
-                    </Typography>
-                    <form noValidate onSubmit={this.handleSubmit}>
-                        <TextField 
-                        id="email" 
-                        name="email" 
-                        label="Email" 
-                        className={classes.textField}
-                        helperText={errors.email}
-                        error={errors.email ? true : false}
-                        value={this.state.email} 
-                        onChange={this.handleChange} 
-                        fullWidth/>
-                         <TextField 
-                        id="password" 
-                        name="password" 
-                        label="Password" 
-                        className={classes.textField}
-                        helperText={errors.password}
-                        error={errors.password ? true : false}
-                        value={this.state.password} 
-                        onChange={this.handleChange} 
-                        fullWidth/>
-                            <Button type="submit" variant="contained" color="primary" className={classes.button}>
-                                Login
-                                </Button>
-                    </form>
+  render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
+    return (
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} alt="monkey" className={classes.image} />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              label="Email"
+              className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              label="Password"
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
 
-                </Grid>
-                <Grid item sm/>
-            </Grid>
-        )
-    }
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              disabled={loading}
+            >
+              Login
+              {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+              )}
+            </Button>
+            <br />
+            <small>
+              don't have an account? sign up <Link to="/signup">here</Link>
+            </small>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
+    );
+  }
 }
 
 login.propTypes = {
-    classes: PropTypes.object.isRequired
-}
+  classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(login);
