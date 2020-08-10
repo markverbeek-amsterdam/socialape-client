9074d2c74d143ab591335b7b019ca82f9591c1a4
Mark Verbeek <markverbeek@mac.com>
2020-08-10 15:44:08 +0200

    redux auth

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import jwtDecode from "jwt-decode";
 //Redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { SET_AUTHENTICATED } from './redux/types'
+import { logoutUser, getUserData } from './redux/actions/userActions'
 
 // Components
 import Navbar from "./components/Navbar";
@@ -27,16 +29,17 @@ const theme = createMuiTheme(themeFile);
 axios.defaults.baseURL =
   "https://europe-west1-socialape-d3a4c.cloudfunctions.net/api";
 
-let authenticated;
 
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logoutUser())
     window.location.href = "/login";
-    authenticated = false;
   } else {
-    authenticated = true;
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common['Authorization'] = token;
+    store.dispatch(getUserData());
   }
 }
 
@@ -54,13 +57,13 @@ class App extends Component {
                   exact
                   path="/login"
                   component={login}
-                  authenticated={authenticated}
+                  
                 />
                 <AuthRoute
                   exact
                   path="/signup"
                   component={signup}
-                  authenticated={authenticated}
+                  
                 />
               </Switch>
             </div>
