6c2776f71ff974bc302a5a1b32143cbb41ae6b41
Mark Verbeek <markverbeek@mac.com>
2020-08-13 11:52:13 +0200

    fixed double keys in profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -62,45 +62,8 @@ const styles = (theme) => ({
     borderBottom: '1px solid rgba(0,0,0,0.1)',
     marginBottom: 20
   },
-  paper: {
-    padding: 20
-  },
-  profile: {
-    '& .image-wrapper': {
-      textAlign: 'center',
-      position: 'relative',
-      '& button': {
-        position: 'absolute',
-        top: '80%',
-        left: '70%'
-      }
-    },
-    '& .profile-image': {
-      width: 200,
-      height: 200,
-      objectFit: 'cover',
-      maxWidth: '100%',
-      borderRadius: '50%'
-    },
-    '& .profile-details': {
-      textAlign: 'center',
-      '& span, svg': {
-        verticalAlign: 'middle'
-      },
-      '& a': {
-        color: '#00bcd4'
-      }
-    },
-    '& hr': {
-      border: 'none',
-      margin: '0 0 10px 0'
-    },
-    '& svg.button': {
-      '&:hover': {
-        cursor: 'pointer'
-      }
-    }
-  },
+
+
   buttons: {
     textAlign: 'center',
     '& a': {
@@ -146,12 +109,7 @@ const styles = (theme) => ({
       },
     },
   },
-  buttons: {
-    textAlign: "center",
-    "& a": {
-      margin: "20px 10px",
-    },
-  }
+  
 
 });
 
